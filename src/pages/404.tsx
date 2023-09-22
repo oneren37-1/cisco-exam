492ad41c50fcaae4173b1a7a7ae1234bc9e9cf5d
oneren37-1 <oneren38@gmail.com>
2023-09-22 14:47:01 +0300

    some

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,7 +7,7 @@ const NotFoundPage = () => {
         <Result
             status="404"
             title="404"
-            subTitle="Извините, страница, которую вы ищете, не существует."
+            subTitle="Извините, страница, которую вы ищете, не существует"
             extra={<Button type="primary"><Link to={'/'}>На главную</Link></Button>}
         />
     );
