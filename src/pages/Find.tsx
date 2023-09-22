2782897b9222f70b0cc9229c543b0d640b2aa8b9
oneren37-1 <oneren38@gmail.com>
2023-09-22 20:36:19 +0300

    suggest

--- src/pages/Find.tsx
+++ src/pages/Find.tsx
@@ -1,21 +1,45 @@
 import React from 'react';
 import PageLayout from "../components/PageLayout/PageLayout";
-import {Card, Empty, Input} from "antd";
+import {Button, Card, Empty, Input} from "antd";
 import data from '../all.json';
 import {LinkOutlined} from "@ant-design/icons";
 
 const Find: React.FC = () => {
-
     const [search, setSearch] = React.useState<string>('');
     const [cards, setCards] = React.useState<any[]>([]);
 
+    const [suggest, setSuggest] = React.useState<string[]>([])
+
+    React.useEffect(() => {
+        console.log(suggest)
+    }, [suggest])
+
     React.useEffect(() => {
         if (search.length < 3) return;
+        const suggestions: any = {}
 
         const filtered = data.filter(el => {
-            return el.question.toLowerCase().includes(search.toLowerCase())
-                || el.variants.some((answer:any) => answer.toLowerCase().includes(search.toLowerCase()))
+            let r = false;
+
+            if (el.question.toLowerCase().includes(search.toLowerCase())) {
+                const cutted = el.question.toLowerCase().substr(el.question.toLowerCase().indexOf(search.toLowerCase()) + search.length)
+                const newSuggestion = cutted.substr(0, Math.max(cutted.indexOf(' '), 0)).trim()
+                suggestions[newSuggestion] = suggestions[newSuggestion] ? suggestions[newSuggestion]+1 : 1;
+                r = true
+            }
+
+            return el.variants.some((answer:any) => {
+                if (answer.toLowerCase().includes(search.toLowerCase())) {
+                    const cutted = answer.toLowerCase().substr(answer.toLowerCase().indexOf(search.toLowerCase()) + search.length)
+                    const newSuggestion = cutted.substr(0, Math.max(cutted.indexOf(' '), 0)).trim()
+                    suggestions[newSuggestion] = suggestions[newSuggestion] ? suggestions[newSuggestion]+1 : 1;
+                    return true
+                }
+            }) || r
         });
+        const s = Object.keys(suggestions).filter(e => e)
+        s.sort((a, b) => suggestions[b] - suggestions[a])
+        setSuggest(s)
         setCards(filtered);
     }, [search]);
 
@@ -23,14 +47,27 @@ const Find: React.FC = () => {
         <PageLayout>
             <Input.Search
                 placeholder="search"
-                enterButton="Search"
+                // enterButton="Search"
                 size="large"
-                onSearch={value => setSearch(value)}
-                onChange={e => setSearch(e.target.value)}
+                onSearch={value => {
+                    setSearch(value)
+                }}
+                onChange={e => {
+                    setSearch(e.target.value)
+                }}
                 style={{
                     marginTop: '50px'
                 }}
+                value={search}
             />
+
+            <div>
+                {suggest.slice(0, 15).map(el => (
+                    <Button
+                        onClick={() => setSearch(search + el + ' ')}
+                    >{el}</Button>
+                ))}
+            </div>
 
             {cards.length > 0 && cards.slice(0, 10).map(el => (
                 <Card key={el.id} style={{
